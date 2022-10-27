cc5ed7702e0df668e7c78691a99981c23fac3cf4
balasitheaswaranb <106142619+balasitheaswaranb@users.noreply.github.com>
2022-10-27 15:34:26 +0530

    final commit

--- 1.Contact_Application/src/com/contact/view/ContactApp.java
+++ 1.Contact_Application/src/com/contact/view/ContactApp.java
@@ -2,6 +2,11 @@ package com.contact.view;
 
 public class ContactApp {
 	public static void main(String[] args) {
+		ContactApp contact = new ContactApp();
+		contact.start();
+	}
+
+	private void start() {
 		MainView mainView = new MainView();
 		mainView.initiate();
 	}
